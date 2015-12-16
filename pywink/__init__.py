e85c6ab4ad03d5969a4f42b5b176b7563b672cea
William Scanlon <scanlonw1@gmail.com>
2015-12-16 13:29:23 -0500

    Added init to WinkEggTray

--- pywink/__init__.py
+++ pywink/__init__.py
@@ -161,6 +161,9 @@ class WinkEggTray(WinkDevice):
 }
 
 """
+    def __init__(self, device_state_as_json, objectprefix="eggtrays"):
+        super(WinkEggTray, self).__init__(device_state_as_json,
+                                            objectprefix=objectprefix)
     def __repr__(self):
         return "<Wink eggtray Name:%s Device_id:%s state:%s>" % (self.name(), self.device_id(), self.state())
 
