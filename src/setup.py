cdd6b8c246108593da5bb1a3eb43a6c2af932081
bradsk88 <7kcwKg60tdcW>
2016-04-05 21:34:50 -0600

    Can now require desired_state to have been reached before updating state

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='python-wink',
-      version='0.7.3',
+      version='0.7.3.2',
       description='Access Wink devices via the Wink API',
       url='http://github.com/bradsk88/python-wink',
       author='Brad Johnson',
